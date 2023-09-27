b7ea8ced0658bb7d95b3689913b7a5ec439f2087
LawYJ <lawyj-wp21@student.tarc.edu.my>
2023-09-28 02:36:09 +0800

    add database but not complete yet

--- app/src/main/java/com/example/studentassigntutor/MainActivity.kt
+++ app/src/main/java/com/example/studentassigntutor/MainActivity.kt
@@ -1,13 +1,48 @@
 package com.example.studentassigntutor
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.*
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var databaseReference: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize the database reference
+        databaseReference = FirebaseDatabase.getInstance().reference
+
+        // Write data to the database
+        writeDataToDatabase("Hello, Firebase!")
+
+        // Read data from the database
+        readDataFromDatabase()
     }
 
+    private fun writeDataToDatabase(data: String) {
+        val dataRef = databaseReference.child("exampleData")
+        dataRef.setValue(data)
+    }
 
+    private fun readDataFromDatabase() {
+        val dataRef = databaseReference.child("exampleData")
+        dataRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.getValue(String::class.java)
+                if (value != null) {
+                    // Data has been read from the database, do something with it
+                    // For example, you can display it in a TextView
+                    // textView.text = value
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle any errors
+            }
+        })
+    }
 }
---
+++ app/src/main/java/com/example/studentassigntutor/TutorTimetable.java
@@ -0,0 +1,5 @@
+package com.example.studentassigntutor;
+
+public class TutorTimetable {
+
+}
